360918f5f41c3ed00512ec53d6282f8da735029a
James Bielicke <j.bielicke@ufl.edu>
2021-06-27 15:15:04 -0500

    Add html, css and js updates Adding all html layout & css styles. Adding js info input.

---
+++ index.js
@@ -0,0 +1,22 @@
+var weather = {
+    "apiKey": "3883aac7fce1e7e9ff348e06b041a68b",
+    fetchWeather: function (city) {
+        fetch("http://api.openweathermap.org/data/2.5/weather?q=" 
+        + city 
+        + "&appid=" 
+        + this.apiKey
+        )
+        .then((response) => response.json())
+        .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function(data) {
+        var { name } = data;
+        var {icon, description } = data.weather[0];
+        var { temp, humidity} = data.main;
+        console.log(name, icon, description, temp, humidity)
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/"+ icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°";
+    },
+};
